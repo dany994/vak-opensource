0f5d67854cde655459f34e8cfbc943e93319d69c
serge.vakulenko <serge.vakulenko@f4aa9954-0cdb-11de-b8f3-b9fb9eb71b04>
2009-07-30 12:55:19 +0000

    NM5101: new phrase for IPMCE.

--- trunk/utilities/nm5101/io2313.h
+++ trunk/utilities/nm5101/io2313.h
@@ -104,7 +104,7 @@
 #define GIMSK     0x3B
 
 /* Stack Pointer */
-#define SP         0x3D 
+#define SP         0x3D
 #define SPL        0x3D
 
 /* Status REGister */
@@ -129,12 +129,12 @@
 #define    UART_TX_vect        (0x12)
 #define    ANA_COMP_vect        (0x14)
 
- 
+
 /*
    The Register Bit names are represented by their bit number (0-7).
-*/     
- 
-/* Stack Pointer */ 
+*/
+
+/* Stack Pointer */
 #define    SP7     7
 #define    SP6     6
 #define    SP5     5
@@ -143,46 +143,46 @@
 #define    SP2     2
 #define    SP1     1
 #define    SP0     0
- 
+
 /* General Interrupt MaSK register */
 #define    INT1    7
 #define    INT0    6
- 
+
 /* General Interrupt Flag Register */
 #define    INTF1   7
 #define    INTF0   6
- 
-/* Timer/Counter Interrupt MaSK register */                 
+
+/* Timer/Counter Interrupt MaSK register */
 #define    TOIE1   7
 #define    OCIE1A  6
 #define    TICIE   3
 #define    TOIE0   1
- 
-/* Timer/Counter Interrupt Flag register */                   
+
+/* Timer/Counter Interrupt Flag register */
 #define    TOV1    7
 #define    OCF1A   6
 #define    ICF1    3
 #define    TOV0    1
- 
-/* MCU general Control Register */ 
+
+/* MCU general Control Register */
 #define    SE      5
 #define    SM      4
 #define    ISC11   3
 #define    ISC10   2
 #define    ISC01   1
 #define    ISC00   0
- 
+
 /* Timer/Counter 0 Control Register */
 #define    CS02    2
 #define    CS01    1
 #define    CS00    0
- 
+
 /* Timer/Counter 1 Control Register */
 #define    COM1A1  7
 #define    COM1A0  6
 #define    PWM11   1
 #define    PWM10   0
- 
+
 /* Timer/Counter 1 Control and Status Register */
 #define    ICNC1   7
 #define    ICES1   6
@@ -190,20 +190,20 @@
 #define    CS12    2
 #define    CS11    1
 #define    CS10    0
-                        
+
 /* Watchdog Timer Control Register */
 #define    WDTOE   4
 #define    WDE     3
 #define    WDP2    2
 #define    WDP1    1
 #define    WDP0    0
- 
+
 /* EEPROM Control Register */
 #define    EEMWE   2
 #define    EEWE    1
 #define    EERE    0
- 
-/* Data Register, Port B */  
+
+/* Data Register, Port B */
 #define    PB7     7
 #define    PB6     6
 #define    PB5     5
@@ -212,7 +212,7 @@
 #define    PB2     2
 #define    PB1     1
 #define    PB0     0
- 
+
 /* Data Direction Register, Port B */
 #define    DDB7    7
 #define    DDB6    6
@@ -222,7 +222,7 @@
 #define    DDB2    2
 #define    DDB1    1
 #define    DDB0    0
- 
+
 /* Input Pins, Port B */
 #define    PINB7   7
 #define    PINB6   6
@@ -232,7 +232,7 @@
 #define    PINB2   2
 #define    PINB1   1
 #define    PINB0   0
- 
+
 /* Data Register, Port D */
 #define    PD6     6
 #define    PD5     5
@@ -241,7 +241,7 @@
 #define    PD2     2
 #define    PD1     1
 #define    PD0     0
- 
+
 /* Data Direction Register, Port D */
 #define    DDD6    6
 #define    DDD5    5
@@ -250,7 +250,7 @@
 #define    DDD2    2
 #define    DDD1    1
 #define    DDD0    0
- 
+
 /* Input Pins, Port D */
 #define    PIND6   6
 #define    PIND5   5
@@ -259,7 +259,7 @@
 #define    PIND2   2
 #define    PIND1   1
 #define    PIND0   0
- 
+
 /* UART Status Register */
 #define    RXC     7
 #define    TXC     6
@@ -267,7 +267,7 @@
 #define    FE      4
 #define    OVR     3    /*This definition differs from the databook    */
                         /*definition to avoid problems with the OR instruction */
- 
+
 /* UART Control Register */
 #define    RXCIE   7
 #define    TXCIE   6
@@ -277,8 +277,8 @@
 #define    CHR9    2
 #define    RXB8    1
 #define    TXB8    0
-       
-/* Analog Comparator Control and Status Register */ 
+
+/* Analog Comparator Control and Status Register */
 #define    ACD     7
 #define    ACO     5
 #define    ACI     4
@@ -286,16 +286,16 @@
 #define    ACIC    2
 #define    ACIS1   1
 #define    ACIS0   0
-       
-/* Pointer definition   */ 
+
+/* Pointer definition   */
 #define    XL     R26
 #define    XH     R27
 #define    YL     R28
 #define    YH     R29
 #define    ZL     R30
 #define    ZH     R31
-       
-/* Constants */ 
+
+/* Constants */
 #define    RAMEND    0xDF
 #define    XRAMEND   0xDF
 #define    E2END     0x7F
