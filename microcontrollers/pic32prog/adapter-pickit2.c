5418513fcffb3f892e7c8f29a11352714bc9d184
serge.vakulenko <serge.vakulenko@f4aa9954-0cdb-11de-b8f3-b9fb9eb71b04>
2011-06-02 06:12:32 +0000

    Detected pic32.

--- microcontrollers/pic32prog/adapter-pickit2.c
+++ microcontrollers/pic32prog/adapter-pickit2.c
@@ -32,6 +32,8 @@ typedef struct {
     /* Доступ к устройству через libusb. */
     usb_dev_handle *usbdev;
 
+    unsigned char reply [64];
+
 } pickit2_adapter_t;
 
 /*
@@ -46,24 +48,56 @@ typedef struct {
 #define OUT_EP                  0x01
 #define IN_EP                   0x81
 
+#define IFACE                   0
 #define TIMO_MSEC               1500
 
 static void pickit2_send (pickit2_adapter_t *a, unsigned argc, ...)
 {
     va_list ap;
     unsigned i;
-    char buf [64];
+    unsigned char buf [64];
 
     memset (buf, CMD_END_OF_BUFFER, 64);
     va_start (ap, argc);
     for (i=0; i<argc; ++i)
         buf[i] = va_arg (ap, int);
     va_end (ap);
-    if (usb_interrupt_write (a->usbdev, OUT_EP, buf, 64, TIMO_MSEC) < 0) {
+    if (debug_level > 0) {
+        int k;
+        fprintf (stderr, "---Send");
+        for (k=0; k<i; ++k) {
+            if (k != 0 && (k & 15) == 0)
+                fprintf (stderr, "\n       ");
+            fprintf (stderr, " %02x", buf[k]);
+        }
+        fprintf (stderr, "\n");
+    }
+    if (usb_interrupt_write (a->usbdev, OUT_EP, (char*) buf, 64, TIMO_MSEC) < 0) {
         fprintf (stderr, "PICkit2: error sending packet\n");
-        usb_release_interface (a->usbdev, 0);
+        usb_release_interface (a->usbdev, IFACE);
         usb_close (a->usbdev);
         exit (-1);
+    }
+}
+
+static void pickit2_recv (pickit2_adapter_t *a)
+{
+    if (usb_interrupt_read (a->usbdev, IN_EP, (char*) a->reply,
+            64, TIMO_MSEC) != 64) {
+        fprintf (stderr, "PICkit2: error receiving packet\n");
+        usb_release_interface (a->usbdev, IFACE);
+        usb_close (a->usbdev);
+        exit (-1);
+    }
+    if (debug_level > 0) {
+        int k;
+        fprintf (stderr, "--->>>>");
+        for (k=0; k<64; ++k) {
+            if (k != 0 && (k & 15) == 0)
+                fprintf (stderr, "\n       ");
+            fprintf (stderr, " %02x", a->reply[k]);
+        }
+        fprintf (stderr, "\n");
     }
 }
 
@@ -71,7 +105,34 @@ static void pickit2_close (adapter_t *adapter)
 {
     pickit2_adapter_t *a = (pickit2_adapter_t*) adapter;
 
-    usb_release_interface (a->usbdev, 0);
+    /* Exit programming mode. */
+    pickit2_send (a, 18, CMD_CLEAR_UPLOAD_BUFFER, CMD_EXECUTE_SCRIPT, 15,
+        SCRIPT_JT2_SETMODE, 5, 0x1f,
+        SCRIPT_VPP_OFF,
+        SCRIPT_MCLR_GND_ON,
+        SCRIPT_VPP_PWM_OFF,
+        SCRIPT_SET_ICSP_PINS, 6,
+        SCRIPT_SET_ICSP_PINS, 2,
+        SCRIPT_SET_ICSP_PINS, 3,
+        SCRIPT_DELAY_LONG, 10,
+        SCRIPT_BUSY_LED_OFF);
+
+    /* Detach power from the board. */
+    pickit2_send (a, 4, CMD_EXECUTE_SCRIPT, 2,
+        SCRIPT_VDD_OFF,
+        SCRIPT_VDD_GND_ON);
+
+    /* Disable reset. */
+    pickit2_send (a, 3, CMD_EXECUTE_SCRIPT, 1,
+        SCRIPT_MCLR_GND_OFF);
+
+    /* Read board status. */
+    pickit2_send (a, 2, CMD_CLEAR_UPLOAD_BUFFER, CMD_READ_STATUS);
+    pickit2_recv (a);
+fprintf (stderr, "PICkit2: status %02x%02x\n", a->reply[1], a->reply[0]);
+
+//fprintf (stderr, "PICkit2: close\n");
+    usb_release_interface (a->usbdev, IFACE);
     usb_close (a->usbdev);
     free (a);
 }
@@ -167,28 +228,109 @@ found:
     }
     if (usb_set_configuration (a->usbdev, 2) < 0) {
         fprintf (stderr, "PICkit2: cannot set USB configuration\n");
-failed: usb_release_interface (a->usbdev, 0);
+failed: usb_release_interface (a->usbdev, IFACE);
         usb_close (a->usbdev);
         free (a);
         return 0;
     }
-    if (usb_claim_interface (a->usbdev, 0) < 0) {
+    if (usb_claim_interface (a->usbdev, IFACE) < 0) {
         fprintf (stderr, "PICkit2: cannot claim USB interface\n");
         goto failed;
     }
 
-    pickit2_send (a, 1, CMD_GET_VERSION);
+    /* Read version of adapter. */
+    pickit2_send (a, 2, CMD_CLEAR_UPLOAD_BUFFER, CMD_GET_VERSION);
+    pickit2_recv (a);
+    fprintf (stderr, "PICkit2: Version %d.%d.%d\n",
+        a->reply[0], a->reply[1], a->reply[2]);
 
-    char reply [64];
-    if (usb_interrupt_read (a->usbdev, IN_EP, reply, 64, TIMO_MSEC) != 64) {
-        fprintf (stderr, "PICkit2: cannot get adapter version\n");
+    /* Detach power from the board. */
+    pickit2_send (a, 4, CMD_EXECUTE_SCRIPT, 2,
+        SCRIPT_VDD_OFF,
+        SCRIPT_VDD_GND_ON);
+
+    /* Setup power voltage 3.3V, fault limit 2.81V. */
+    unsigned vdd = (unsigned) (3.3 * 32 + 10.5) << 6;
+    unsigned vdd_limit = (unsigned) ((2.81 / 5) * 255);
+    pickit2_send (a, 4, CMD_SET_VDD, vdd, vdd >> 8, vdd_limit);
+
+    /* Setup reset voltage 3.28V, fault limit 2.26V. */
+    unsigned vpp = (unsigned) (3.28 * 18.61);
+    unsigned vpp_limit = (unsigned) (2.26 * 18.61);
+    pickit2_send (a, 4, CMD_SET_VPP, 0x40, vpp, vpp_limit);
+
+    /* Setup serial speed. */
+    unsigned divisor = 16;
+    pickit2_send (a, 4, CMD_EXECUTE_SCRIPT, 2,
+        SCRIPT_SET_ICSP_SPEED, divisor);
+
+    /* Reset active low. */
+    pickit2_send (a, 3, CMD_EXECUTE_SCRIPT, 1,
+        SCRIPT_MCLR_GND_ON);
+
+    /* Read board status. */
+    pickit2_send (a, 2, CMD_CLEAR_UPLOAD_BUFFER, CMD_READ_STATUS);
+    pickit2_recv (a);
+    unsigned status = a->reply[0] | a->reply[1] << 8;
+fprintf (stderr, "PICkit2: status %04x\n", status);
+    if (status != (STATUS_VDD_GND_ON | STATUS_VPP_GND_ON)) {
+        fprintf (stderr, "PICkit2: invalid status = %04x\n", status);
         goto failed;
     }
-    fprintf (stderr, "Found PICkit 2 adapter: Version %d.%d.%d\n",
-        (unsigned char) reply[0], (unsigned char) reply[1],
-        (unsigned char) reply[2]);
 
-    /* Обязательные функции. */
+    /* Enable power to the board. */
+    pickit2_send (a, 4, CMD_EXECUTE_SCRIPT, 2,
+        SCRIPT_VDD_GND_OFF,
+        SCRIPT_VDD_ON);
+
+    /* Read board status. */
+    pickit2_send (a, 2, CMD_CLEAR_UPLOAD_BUFFER, CMD_READ_STATUS);
+    pickit2_recv (a);
+    status = a->reply[0] | a->reply[1] << 8;
+fprintf (stderr, "PICkit2: status %04x\n", status);
+    if (status != (STATUS_VDD_ON | STATUS_VPP_GND_ON)) {
+        fprintf (stderr, "PICkit2: invalid status = %04x.\n", status);
+        goto failed;
+    }
+
+    /* Enter programming mode. */
+    pickit2_send (a, 42, CMD_CLEAR_UPLOAD_BUFFER, CMD_EXECUTE_SCRIPT, 39,
+        SCRIPT_VPP_OFF,
+        SCRIPT_MCLR_GND_ON,
+        SCRIPT_VPP_PWM_ON,
+        SCRIPT_BUSY_LED_ON,
+        SCRIPT_SET_ICSP_PINS, 0,
+        SCRIPT_DELAY_LONG, 20,
+        SCRIPT_MCLR_GND_OFF,
+        SCRIPT_VPP_ON,
+        SCRIPT_DELAY_SHORT, 23,
+        SCRIPT_VPP_OFF,
+        SCRIPT_MCLR_GND_ON,
+        SCRIPT_DELAY_SHORT, 47,
+        SCRIPT_WRITE_BYTE_LITERAL, 0xb2,
+        SCRIPT_WRITE_BYTE_LITERAL, 0xc2,
+        SCRIPT_WRITE_BYTE_LITERAL, 0x12,
+        SCRIPT_WRITE_BYTE_LITERAL, 0x0a,
+        SCRIPT_MCLR_GND_OFF,
+        SCRIPT_VPP_ON,
+        SCRIPT_DELAY_SHORT, 235,
+        SCRIPT_SET_ICSP_PINS, 2,
+        SCRIPT_JT2_SETMODE, 6, 0x1f,
+        SCRIPT_JT2_SENDCMD, 4,
+        SCRIPT_JT2_SENDCMD, 7,
+        SCRIPT_JT2_XFERDATA8_LIT, 0);
+
+    /* Read device id. */
+    pickit2_send (a, 12, CMD_CLEAR_UPLOAD_BUFFER, CMD_EXECUTE_SCRIPT, 9,
+        SCRIPT_JT2_SENDCMD, 4,
+        SCRIPT_JT2_SENDCMD, 1,
+        SCRIPT_JT2_XFERDATA32_LIT, 0, 0, 0, 0);
+    pickit2_send (a, 1, CMD_UPLOAD_DATA);
+    pickit2_recv (a);
+    fprintf (stderr, "PICkit2: read id, %d bytes: %02x %02x %02x %02x\n",
+        a->reply[0], a->reply[1], a->reply[2], a->reply[3], a->reply[4]);
+
+    /* User functions. */
     a->adapter.close = pickit2_close;
     a->adapter.get_idcode = pickit2_get_idcode;
     a->adapter.read_data = pickit2_read_data;
