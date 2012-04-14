31472a9a51703a1d53e57e0527d6ee107f84a152
serge.vakulenko <serge.vakulenko@f4aa9954-0cdb-11de-b8f3-b9fb9eb71b04>
2012-04-14 02:55:43 +0000

    Update arduino ethernet library.

--- microcontrollers/arduino-ethernet/socket.h
+++ microcontrollers/arduino-ethernet/socket.h
@@ -3,32 +3,50 @@
 
 #include "w5100.h"
 
-// Opens a socket(TCP or UDP or IP_RAW mode)
+/*
+ * Opens a socket(TCP or UDP or IP_RAW mode)
+ */
 extern uint8_t socket (socket_t s, uint8_t protocol, uint16_t port, uint8_t flag);
 
-// Close socket
+/*
+ * Close socket
+ */
 extern void close (socket_t s);
 
-// Establish TCP connection (Active connection)
+/*
+ * Establish TCP connection (Active connection)
+ */
 extern uint8_t connect (socket_t s, uint8_t * addr, uint16_t port);
 
-// disconnect the connection
+/*
+ * disconnect the connection
+ */
 extern void disconnect (socket_t s);
 
-// Establish TCP connection (Passive connection)
+/*
+ * Establish TCP connection (Passive connection)
+ */
 extern uint8_t listen (socket_t s);
 
-// Send data (TCP)
+/*
+ * Send data (TCP)
+ */
 extern uint16_t send (socket_t s, const uint8_t * buf, uint16_t len);
 
-// Receive data (TCP)
+/*
+ * Receive data (TCP)
+ */
 extern uint16_t recv (socket_t s, uint8_t * buf, uint16_t len);
 extern uint16_t peek (socket_t s, uint8_t *buf);
 
-// Send data (UDP/IP RAW)
+/*
+ * Send data (UDP/IP RAW)
+ */
 extern uint16_t sendto (socket_t s, const uint8_t * buf, uint16_t len, uint8_t * addr, uint16_t port);
 
-// Receive data (UDP/IP RAW)
+/*
+ * Receive data (UDP/IP RAW)
+ */
 extern uint16_t recvfrom (socket_t s, uint8_t * buf, uint16_t len, uint8_t * addr, uint16_t *port);
 
 extern uint16_t igmpsend (socket_t s, const uint8_t * buf, uint16_t len);
