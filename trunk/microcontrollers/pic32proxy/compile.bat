5eb6846a739e73f9dee4445ba8ce593a86342825
serge.vakulenko <serge.vakulenko@f4aa9954-0cdb-11de-b8f3-b9fb9eb71b04>
2012-07-21 05:36:16 +0000

    Pic32proxy moved to http://code.google.com/p/ejtagproxy/

--- trunk/microcontrollers/pic32proxy/compile.bat
+++
@@ -1,6 +0,0 @@
-@echo **************************
-@echo *** Use MinGW compiler ***
-@echo **************************
-@set PATH=c:\mingw\bin;d:\mingw\bin;%PATH%
-@set CC=gcc
-mingw32-make.exe -f make-mingw
