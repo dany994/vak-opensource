36bb27aca11d07afc003ad4bf5f28a95102b4476
serge.vakulenko <serge.vakulenko@f4aa9954-0cdb-11de-b8f3-b9fb9eb71b04>
2012-10-29 01:28:47 +0000

    Move keyer to a separate file.

--- hamradio/cwchatbot/test-keyer.c
+++ hamradio/cwchatbot/test-keyer.c
@@ -17,121 +17,26 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <getopt.h>
-#include <math.h>
 
 #include "paddle.h"
 #include "audio.h"
+#include "keyer.h"
 
 char *progname;
 int wpm = 12;
 int tone = 800;
-volatile int daah_active, dit_active;
-volatile int action;
-volatile int idle_count = 6;
 
-short dit_data [22000];
-short daah_data [44000];
-int dit_len;
-int daah_len;
-
-typedef struct {
-    const int character;	/* The character represented */
-    const char *representation; /* Dot-dash shape of the character */
-} cw_entry_t;
-
-static const cw_entry_t cw_table[] = {
-    /* ASCII 7bit letters */
-    { 'A', ".-"     }, { 'B', "-..."   }, { 'C', "-.-."   },
-    { 'D', "-.."    }, { 'E', "."      }, { 'F', "..-."   },
-    { 'G', "--."    }, { 'H', "...."   }, { 'I', ".."     },
-    { 'J', ".---"   }, { 'K', "-.-"    }, { 'L', ".-.."   },
-    { 'M', "--"     }, { 'N', "-."     }, { 'O', "---"    },
-    { 'P', ".--."   }, { 'Q', "--.-"   }, { 'R', ".-."    },
-    { 'S', "..."    }, { 'T', "-"      }, { 'U', "..-"    },
-    { 'V', "...-"   }, { 'W', ".--"    }, { 'X', "-..-"   },
-    { 'Y', "-.--"   }, { 'Z', "--.."   },
-
-    /* Numerals */
-    { '0', "-----"  }, { '1', ".----"  }, { '2', "..---"  },
-    { '3', "...--"  }, { '4', "....-"  }, { '5', "....."  },
-    { '6', "-...."  }, { '7', "--..."  }, { '8', "---.."  },
-    { '9', "----."  },
-
-    /* Punctuation */
-    { '"', ".-..-." }, { '\'',".----." }, { '$', "...-..-"},
-    { '(', "-.--."  }, { ')', "-.--.-" }, { '+', ".-.-."  },
-    { ',', "--..--" }, { '-', "-....-" }, { '.', ".-.-.-" },
-    { '/', "-..-."  }, { ':', "---..." }, { ';', "-.-.-." },
-    { '=', "-...-"  }, { '?', "..--.." }, { '_', "..--.-" },
-    { '@', ".--.-." },
-};
-
-static const int cw_table_size = sizeof(cw_table) / sizeof(cw_entry_t);
-
-void decode (int element)
+static void quit ()
 {
-    static char word [16];
-    static int nbits = 0;
-    int i;
-
-    if (element != ' ' && nbits < sizeof(word)-1) {
-        /* Append element to the word. */
-        word [nbits++] = element;
-        printf ("%c", action);
-        return;
-    }
-    if (nbits == 0)
-        return;
-
-    /* Compare with morse table. */
-    word[nbits] = '\0';
-    nbits = 0;
-    for (i=0; i<cw_table_size; i++) {
-        //printf ("comparing sign %d...\n", i);
-        if (strcmp (cw_table[i].representation, word) == 0) {
-            printf ("[%c]", cw_table[i].character);
-            return;
-        }
-    }
-    printf ("[??]");
-}
-
-/*
- * Fill the array with sinusoidal data
- */
-static void fill_data (short *data, int len, int samples_per_cycle)
-{
-    int n;
-
-    /* First cycle. */
-    for (n=0; n<samples_per_cycle; ++n) {
-        data[n] = 20000 * sin (2*M_PI*n / samples_per_cycle);
-    }
-
-    /* Next cycles. */
-    for (; n<len; n+=samples_per_cycle) {
-        memcpy (data + n, data, samples_per_cycle * sizeof(data[0]));
-    }
-
-    /* Make slow rising and falling edges. */
-    int ncycles = 3 * samples_per_cycle;
-    double phi = M_PI / ncycles;
-
-    for (n=0; n<ncycles; ++n) {
-        double factor = (1 - cos (n * phi)) / 2;
-
-        data [n] = lround (data [n] * factor);
-        data [len - 1 - n + ncycles] = lround (data [len - 1 - n] * factor);
-    }
+    audio_stop();
+    paddle_close();
+    exit (0);
 }
 
 static void sigint (int sig)
 {
-    audio_stop();
-    paddle_close();
-    exit (-1);
+    quit();
 }
 
 void usage ()
@@ -172,16 +77,9 @@ int main (int argc, char **argv)
     if (argc != 0 || wpm == 0)
         usage ();
 
-    int sample_rate = audio_init();
-    int samples_per_cycle = sample_rate / tone / 2 * 2;
-    dit_len = sample_rate * 12 / wpm / 10 /
-              samples_per_cycle * samples_per_cycle;
-    daah_len = 3 * dit_len;
-    fill_data (dit_data, dit_len, samples_per_cycle);
-    fill_data (daah_data, daah_len, samples_per_cycle);
-
+    keyer_init (tone, wpm);
     paddle_open();
-    atexit (paddle_close);
+    atexit (quit);
     signal (SIGINT, sigint);
 
     printf ("Keyer ready at %d wpm, tone %d Hz.\n", wpm, tone);
@@ -190,51 +88,6 @@ int main (int argc, char **argv)
     for (;;) {
         int daah = 0, dit = 0;
         paddle_poll (&daah, &dit);
-        dit_active = dit;
-        daah_active = daah;
-
-        if (action) {
-            decode (action);
-            fflush (stdout);
-            action = 0;
-        }
-        if (idle_count == 5) {
-            idle_count++;
-            printf (" ");
-            fflush (stdout);
-        }
+        keyer_decode (daah, dit);
     }
-    audio_stop();
-    paddle_close();
-    return (0);
-}
-
-/*
- * Fill the data array with audio samples.
- * Return the number of samples.
- */
-int audio_output (short *data, int maxdata)
-{
-    if (dit_active) {
-        /* Play a dot and one inter-sign interval. */
-        action = '.';
-        idle_count = 0;
-        memcpy (data, dit_data, (dit_len + dit_len) * sizeof(data[0]));
-        return dit_len + dit_len;
-    }
-    if (daah_active) {
-        action = '-';
-        idle_count = 0;
-        memcpy (data, daah_data, (daah_len + dit_len) * sizeof(data[0]));
-        return daah_len + dit_len;
-    }
-    idle_count++;
-    if (idle_count == 1) {
-        action = ' ';
-        idle_count++;
-        memset (data, 0, (dit_len + dit_len) * sizeof(data[0]));
-        return dit_len + dit_len;
-    }
-    memset (data, 0, dit_len * sizeof(data[0]));
-    return dit_len;
 }
