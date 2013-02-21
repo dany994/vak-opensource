5fce12975f31de33cf5b4e730d4b17c134126d7e
serge.vakulenko <serge.vakulenko@f4aa9954-0cdb-11de-b8f3-b9fb9eb71b04>
2013-02-21 21:49:19 +0000

    SystemC: added an example of Verilog cosimulation.

---
+++ hardware/systemc/counter-verilog/counter.v
@@ -0,0 +1,21 @@
+`timescale 1ns / 1ps
+//
+// Negate-or function.
+//
+
+module counter_m (
+    input wire clock,
+    input wire reset,
+    input wire enable,
+    output reg [3:0] counter_out
+);
+
+    always @(posedge clock) begin
+        if (reset)
+            counter_out <= 0;
+        else if (enable) begin
+            counter_out <= counter_out + 1;
+            $display("(%0d ns) Incremented Counter %d", $time, counter_out);
+        end
+    end
+endmodule
