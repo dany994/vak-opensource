06ea7c03712ef3954f2b33c915482d369dd46d1a
serge.vakulenko <serge.vakulenko@f4aa9954-0cdb-11de-b8f3-b9fb9eb71b04>
2013-02-24 04:29:43 +0000

    Rtlsim example compiled.

--- trunk/hardware/rtlsim/rtlsim.c
+++ trunk/hardware/rtlsim/rtlsim.c
@@ -2,6 +2,7 @@
 // Simple RTL simulator.
 // Copyright (C) 2013 Serge Vakulenko <serge@vak.ru>
 //
+#include <stdio.h>
 #include "rtlsim.h"
 
 //
@@ -15,10 +16,21 @@ void signal_set (signal_t *sig, value_t value)
 
     if (value != sig->value && sig->next == 0) {
         // Value changed - put to list of active signals
-        // TODO: check edge
+        printf ("(%llu) Signal '%s' activated\n", time_ticks, sig->name);
         sig->next = signal_active;
         signal_active = sig;
     }
+}
+
+static int edge_is_sensitive (signal_t *sig, hook_t *hook)
+{
+    if (hook->edge == 0)
+        return 1;
+    if ((hook->edge & POSEDGE) && sig->value == 0 && sig->new_value != 0)
+        return 1;
+    if ((hook->edge & NEGEDGE) && sig->value != 0 && sig->new_value == 0)
+        return 1;
+    return 0;
 }
 
 //
@@ -29,31 +41,47 @@ void process_wait (void)
 {
     process_t *old = process_current;
 
-    // Schedule processes for active signals
-    for (; signal_active != 0; signal_active = signal_active->next) {
-        hook_t *hook = signal_active->activate;
-
-        // Handle all processes, sensitive to this signal
-        for (; hook != 0; hook = hook->next) {
-            if (hook->process->next == 0) {
-                // Put the process to queue of pending events.
-                hook->process->next = process_queue;
-                process_queue = hook->process;
-            }
-        }
-    }
     if (process_queue == 0) {
         // Cannot happen.
         printf ("Internal error: empty process queue\n");
         exit (-1);
     }
+    if (process_current->delay != 0) {
+        // Delta cycle finished.
+        printf ("(%llu) Delta cycle finished\n", time_ticks);
+
+        // Schedule processes for active signals.
+        while (signal_active != 0) {
+            hook_t *hook = signal_active->activate;
+            signal_t *next = signal_active->next;
+
+            // Handle all processes, sensitive to this signal.
+            for (; hook != 0; hook = hook->next) {
+                if (hook->process->next == 0 &&
+                    edge_is_sensitive (signal_active, hook))
+                {
+                    // Put the process to queue of pending events.
+                    printf ("(%llu) Process '%s' activated\n", time_ticks, hook->process->name);
+                    hook->process->next = process_queue;
+                    process_queue = hook->process;
+                }
+            }
+            // Setup new value
+            signal_active->value = signal_active->new_value;
+            signal_active->next = 0;
+            signal_active = next;
+        }
+    }
     // Select next process from the queue.
     process_current = process_queue;
     process_queue = process_queue->next;
+    process_current->next = 0;
     if (process_current->delay != 0) {
         // Advance time.
         time_ticks += process_current->delay;
+        printf ("(%llu) Time advanced\n", time_ticks);
     }
+    printf ("(%llu) Switch process '%s' -> '%s'\n", time_ticks, old->name, process_current->name);
     swapcontext(&old->context, &process_current->context);
 }
 
@@ -70,7 +98,7 @@ void process_delay (unsigned ticks)
     }
 
     // Put the current process to queue of pending events.
-    TODO: enqueue (process_current, ticks);
+    //TODO: enqueue (process_current, ticks);
 
     // Switch to next active process.
     process_wait();
