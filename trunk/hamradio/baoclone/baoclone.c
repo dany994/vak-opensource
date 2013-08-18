f60ecb01928d85275a76fd9b06576f27c9559ab5
serge.vakulenko <serge.vakulenko@f4aa9954-0cdb-11de-b8f3-b9fb9eb71b04>
2013-08-18 02:55:32 +0000

    Successfully read the contents of uv-5r memory.

--- trunk/hamradio/baoclone/baoclone.c
+++ trunk/hamradio/baoclone/baoclone.c
@@ -22,13 +22,17 @@
 const char version[] = "1.0";
 const char copyright[] = "Copyright (C) 2013 Serge Vakulenko";
 
-char *progname;
-char *portname;
-int verbose;
-int trace;
-int debug;
+const unsigned char UV5R_MODEL_ORIG[] = "\x50\xBB\xFF\x01\x25\x98\x4D";
+const unsigned char UV5R_MODEL_291[] = "\x50\xBB\xFF\x20\x12\x07\x25";
 
-struct termios oldtio, newtio;
+char *progname;
+int verbose;
+char *portname;                 // Device name of serial port
+
+struct termios oldtio, newtio;  // Mode of serial port
+
+unsigned char ident [8];        // Radio: identifier
+unsigned char mem [0x2000];     // Radio: memory contents
 
 extern char *optarg;
 extern int optind;
@@ -39,12 +43,13 @@ void usage ()
     fprintf (stderr, "Usage:\n\t%s [-vtd] file\n", progname);
     fprintf (stderr, "Options:\n");
     fprintf (stderr, "  -v          verbose mode\n");
-    fprintf (stderr, "  -t          trace mode\n");
-    fprintf (stderr, "  -d          debug\n");
     exit (-1);
 }
 
-void print_hex (const char *data, int len)
+//
+// Print data in hex format.
+//
+void print_hex (const unsigned char *data, int len)
 {
     int i;
 
@@ -60,42 +65,32 @@ int open_port (char *portname)
 {
     int fd;
 
-    // Don't wait for carrier (DCD).
+    // Use non-block flag to ignore carrier (DCD).
     fd = open (portname, O_RDWR | O_NOCTTY | O_NONBLOCK);
     if (fd < 0) {
         perror (portname);
         exit (-1);
     }
 
-    // Flush received data pending on the port.
-    tcflush (fd, TCIFLUSH);
-
+    // Get terminal modes.
     tcgetattr (fd, &oldtio);
     newtio = oldtio;
 
-    // 8 data bits
     newtio.c_cflag &= ~CSIZE;
-    newtio.c_cflag |= CS8;
-    // enable receiver, set local mode
-    newtio.c_cflag |= CLOCAL | CREAD;
-    // no parity
-    newtio.c_cflag &= ~PARENB;
-    // 1 stop bit
-    newtio.c_cflag &= ~CSTOPB;
-    // no h/w handshake
-    newtio.c_cflag &= ~CRTSCTS;
-    // raw input
-    newtio.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
-    // raw output
-    newtio.c_oflag &= ~OPOST;
-    // software flow control disabled
-    newtio.c_iflag &= ~IXON;
-    // do not translate CR to NL
-    newtio.c_iflag &= ~ICRNL;
+    newtio.c_cflag |= CS8;              // 8 data bits
+    newtio.c_cflag |= CLOCAL | CREAD;   // enable receiver, set local mode
+    newtio.c_cflag &= ~PARENB;          // no parity
+    newtio.c_cflag &= ~CSTOPB;          // 1 stop bit
+    newtio.c_cflag &= ~CRTSCTS;         // no h/w handshake
+    newtio.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);  // raw input
+    newtio.c_oflag &= ~OPOST;           // raw output
+    newtio.c_iflag &= ~IXON;            // software flow control disabled
+    newtio.c_iflag &= ~ICRNL;           // do not translate CR to NL
 
-    cfsetispeed(&newtio, 9600);
-    cfsetospeed(&newtio, 9600);
+    cfsetispeed(&newtio, B9600);        // Set baud rate.
+    cfsetospeed(&newtio, B9600);
 
+    // Set terminal modes.
     tcsetattr (fd, TCSANOW, &newtio);
 
     // Clear the non-block flag.
@@ -109,6 +104,9 @@ int open_port (char *portname)
         perror("F_SETFL");
         exit (-1);
     }
+
+    // Flush received data pending on the port.
+    tcflush (fd, TCIFLUSH);
     return fd;
 }
 
@@ -125,67 +123,91 @@ void close_port (int fd)
     usleep (2000000);
 }
 
-int read_with_timeout (int fd, char *data, int len)
+//
+// Read data from serial port.
+// Return 0 when no data available.
+// Use 200-msec timeout.
+//
+int read_with_timeout (int fd, unsigned char *data, int len)
 {
     fd_set rset, wset, xset;
+    int nbytes, len0 = len;
 
-    // Initialize file descriptor sets
-    FD_ZERO (&rset);
-    FD_ZERO (&wset);
-    FD_ZERO (&xset);
-    FD_SET (fd, &rset);
+    for (;;) {
+        // Initialize file descriptor sets.
+        FD_ZERO (&rset);
+        FD_ZERO (&wset);
+        FD_ZERO (&xset);
+        FD_SET (fd, &rset);
 
-    // Set timeout to 100 msec
-    struct timeval timo;
-    timo.tv_sec = 0;
-    timo.tv_usec = 200000;
+        // Set timeout to 100 msec.
+        struct timeval timo;
+        timo.tv_sec = 0;
+        timo.tv_usec = 200000;
 
-    // Wait for input to become ready or until the time out.
-    if (select (fd + 1, &rset, &wset, &xset, &timo) != 1)
-        return 0;
+        // Wait for input to become ready or until the time out.
+        if (select (fd + 1, &rset, &wset, &xset, &timo) != 1)
+            return 0;
 
-    return read (fd, data, len);
+        nbytes = read (fd, (unsigned char*) data, len);
+        if (nbytes <= 0)
+            return 0;
+
+        len -= nbytes;
+        if (len <= 0)
+            return len0;
+
+        data += nbytes;
+    }
 }
 
-int try_magic (int fd, const char *magic)
+//
+// Try to identify the device with a given magic command.
+// Return 0 when failed.
+//
+int try_magic (int fd, const unsigned char *magic)
 {
-    char reply[8];
+    unsigned char reply;
+    int magic_len = strlen ((char*) magic);
 
     // Send magic.
-    //printf ("Sending magic: ");
-    //print_hex (magic, strlen(magic));
-    //printf ("\n");
+    if (verbose) {
+        printf ("Sending magic: ");
+        print_hex (magic, magic_len);
+        printf ("\n");
+    }
     tcflush (fd, TCIFLUSH);
-    write (fd, magic, strlen(magic));
+    write (fd, magic, magic_len);
 
     // Check response.
-    if (read_with_timeout (fd, reply, 1) != 1) {
-        //printf ("Radio did not respond.\n");
+    if (read_with_timeout (fd, &reply, 1) != 1) {
+        if (verbose)
+            printf ("Radio did not respond.\n");
         return 0;
     }
-    if (reply[0] != 0x06) {
-        printf ("Bad response: %02x\n", reply[0]);
+    if (reply != 0x06) {
+        printf ("Bad response: %02x\n", reply);
         return 0;
     }
 
     // Query for identifier..
     write (fd, "\x02", 1);
-    if (read_with_timeout (fd, reply, 8) != 8) {
+    if (read_with_timeout (fd, ident, 8) != 8) {
         printf ("Empty identifier.\n");
         return 0;
     }
     printf ("Identifier: ");
-    print_hex (reply, 8);
+    print_hex (ident, 8);
     printf ("\n");
 
     // Enter clone mode.
     write (fd, "\x06", 1);
-    if (read_with_timeout (fd, reply, 1) != 1) {
+    if (read_with_timeout (fd, &reply, 1) != 1) {
         printf ("Radio refused to clone.\n");
         return 0;
     }
-    if (reply[0] != 0x06) {
-        printf ("Radio refused to clone: %02x\n", reply[0]);
+    if (reply != 0x06) {
+        printf ("Radio refused to clone: %02x\n", reply);
         return 0;
     }
     return 1;
@@ -196,8 +218,6 @@ int try_magic (int fd, const char *magic)
 //
 void identify (int fd)
 {
-    static const char *UV5R_MODEL_ORIG = "\x50\xBB\xFF\x01\x25\x98\x4D";
-    static const char *UV5R_MODEL_291 = "\x50\xBB\xFF\x20\x12\x07\x25";
     int retry;
 
     for (retry=0; retry<10; retry++) {
@@ -217,21 +237,80 @@ void identify (int fd)
     exit (-1);
 }
 
+//
+// Read block of data, up to 64 bytes.
+// Halt the program on any error.
+//
+void read_block (int fd, int start, unsigned char *data, int nbytes)
+{
+    unsigned char cmd[4], reply[4];
+    int addr, len;
+
+    // Send command.
+    cmd[0] = 'S';
+    cmd[1] = start >> 8;
+    cmd[2] = start;
+    cmd[3] = nbytes;
+    write (fd, cmd, 4);
+
+    // Read reply.
+    if (read_with_timeout (fd, reply, 4) != 4) {
+        printf ("Radio refused to send block 0x%04x.\n", start);
+        exit(-1);
+    }
+    addr = reply[1] << 8 | reply[2];
+    if (reply[0] != 'X' || addr != start || reply[3] != nbytes) {
+        printf ("Bad reply for block 0x%04x of %d bytes: %02x-%02x-%02x-%02x\n",
+            start, nbytes, reply[0], reply[1], reply[2], reply[3]);
+        exit(-1);
+    }
+
+    // Read data.
+    len = read_with_timeout (fd, data, 0x40);
+    if (len != nbytes) {
+        printf ("Reading block 0x%04x: got only %d bytes.\n", start, len);
+        exit(-1);
+    }
+
+    // Get acknowledge.
+    write (fd, "\x06", 1);
+    if (read_with_timeout (fd, reply, 1) != 1) {
+        printf ("No acknowledge after block 0x%04x.\n", start);
+        exit(-1);
+    }
+    if (reply[0] != 0x06) {
+        printf ("Bad acknowledge after block 0x%04x: %02x\n", start, reply[0]);
+        exit(-1);
+    }
+    if (verbose) {
+        printf ("0x%04x: ", start);
+        print_hex (data, nbytes);
+        printf ("\n");
+    }
+}
+
+void read_memory (int fd)
+{
+    int addr;
+
+    // Main block.
+    for (addr=0; addr<0x1800; addr+=0x40)
+        read_block (fd, addr, &mem[addr], 0x40);
+
+    // Auxiliary block starts at 0x1EC0.
+    for (addr=0x1EC0; addr<0x2000; addr+=0x40)
+        read_block (fd, addr, &mem[addr], 0x40);
+}
+
 int main (int argc, char **argv)
 {
     progname = *argv;
     for (;;) {
-        switch (getopt (argc, argv, "vtd")) {
+        switch (getopt (argc, argv, "v")) {
         case EOF:
             break;
         case 'v':
             ++verbose;
-            continue;
-        case 't':
-            ++trace;
-            continue;
-        case 'd':
-            ++debug;
             continue;
         default:
             usage ();
@@ -248,6 +327,7 @@ int main (int argc, char **argv)
     int fd = open_port (portname);
 
     identify (fd);
+    read_memory (fd);
 
     // TODO
 
