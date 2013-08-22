4e5547e7dc279a35ad2b88bdf660abf9a099c82b
serge.vakulenko <serge.vakulenko@f4aa9954-0cdb-11de-b8f3-b9fb9eb71b04>
2013-08-22 04:34:40 +0000

    Move device-dependent routines to separate file.

---
+++ trunk/hamradio/baoclone/util.h
@@ -0,0 +1,49 @@
+/*
+ * Auxiliary functions.
+ *
+ * Copyright (C) 2013 Serge Vakulenko, KK6ABQ
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *   1. Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the following disclaimer.
+ *   2. Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *   3. The name of the author may not be used to endorse or promote products
+ *      derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+//
+// Trace data i/o via the serial port.
+//
+int verbose;
+
+//
+// Print data in hex format.
+//
+void print_hex (const unsigned char *data, int len);
+
+//
+// Read data from serial port.
+// Return 0 when no data available.
+// Use 200-msec timeout.
+//
+int read_with_timeout (int fd, unsigned char *data, int len);
+
+//
+// Check for a regular file.
+//
+int is_file (char *filename);
