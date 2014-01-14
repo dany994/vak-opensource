acc8eace50f3eb1536b2b2a7cfc84b3a2c7571bc
serge.vakulenko <serge.vakulenko@f4aa9954-0cdb-11de-b8f3-b9fb9eb71b04>
2014-01-14 08:18:21 +0000

    M-54: opcode decoder verified.

--- trunk/microcontrollers/mk-54/pmktool/opcodes.c
+++ trunk/microcontrollers/mk-54/pmktool/opcodes.c
@@ -30,70 +30,76 @@ typedef struct {
 } opcode_t;
 
 static opcode_t opcodes[] = {
-    { 0x0A, 0, ","	 },
-    { 0x0B, 0, "/-/"	 },
-    { 0x0C, 0, "ВП"	 },
-    { 0x0D, 0, "Сx"	 },
-    { 0x0E, 0, "В^"	 },
-    { 0x0F, 0, "FВx"	 },
-    { 0x10, 0, "+"	 },
-    { 0x11, 0, "-"	 },
-    { 0x12, 0, "x"	 },
-    { 0x13, 0, ":"	 },
-    { 0x14, 0, "<->"	 },
-    { 0x15, 0, "F10^x"	 },
-    { 0x16, 0, "Fe^x"	 },
-    { 0x17, 0, "Flg"	 },
-    { 0x18, 0, "Fln"	 },
-    { 0x19, 0, "Farcsin" },
-    { 0x1A, 0, "Farccos" },
-    { 0x1B, 0, "Farctg"	 },
-    { 0x1C, 0, "Fsin"	 },
-    { 0x1D, 0, "Fcos"	 },
-    { 0x1E, 0, "Ftg"	 },
-    { 0x20, 0, "Fпи"	 },
-    { 0x21, 0, "Kкорень" },
-    { 0x22, 0, "Fx^2"	 },
-    { 0x23, 0, "F1/x"	 },
-    { 0x24, 0, "Fx^y"	 },
-    { 0x25, 0, "F->"	 },
-    { 0x26, 0, "KМГ"	 },
-    { 0x2A, 0, "KМЧ"	 },
-    { 0x30, 0, "KЧМ"	 },
-    { 0x31, 0, "K|x|"	 },
-    { 0x32, 0, "KЗН"	 },
-    { 0x33, 0, "KГМ"	 },
-    { 0x34, 0, "K{x}"	 },
-    { 0x35, 0, "K{x}"	 },
-    { 0x36, 0, "Kmax"	 },
-    { 0x37, 0, "K/\\"	 },
-    { 0x38, 0, "K\\/"	 },
-    { 0x39, 0, "K(+)"	 },
-    { 0x3A, 0, "KИНВ"	 },
-    { 0x3B, 0, "KСЧ"	 },
-    { 0x40, 1, "xП"	 },
-    { 0x50, 0, "С/П"	 },
-    { 0x51, 2, "БП"	 },
-    { 0x52, 0, "В/О"	 },
-    { 0x53, 2, "ПП"	 },
-    { 0x54, 0, "KНОП"	 },
-    { 0x57, 2, "Fx#0"	 },
-    { 0x58, 2, "FL2"	 },
-    { 0x59, 2, "Fx>=0"	 },
-    { 0x5A, 2, "FL3"	 },
-    { 0x5B, 2, "FL1"	 },
-    { 0x5C, 2, "Fx<0"	 },
-    { 0x5D, 2, "FL0"	 },
-    { 0x5E, 2, "Fx=0"	 },
-    { 0x60, 1, "Пx"	 },
-    { 0x70, 1, "Kx#0"	 },
-    { 0x80, 1, "KБП"	 },
-    { 0x90, 1, "Kx>=0"	 },
-    { 0xA0, 1, "KПП"	 },
-    { 0xB0, 1, "KxП"	 },
-    { 0xC0, 1, "Kx<0"	 },
-    { 0xD0, 1, "КПx"	 },
-    { 0xE0, 1, "Kx=0"	 },
+    { 0x0A, 0, ","        },
+    { 0x0B, 0, "/-/"      },
+    { 0x0C, 0, "ВП"       },
+    { 0x0D, 0, "Сx"       },
+    { 0x0E, 0, "В^"       },
+    { 0x0F, 0, "F Вx"     },
+    { 0x10, 0, "+"        },
+    { 0x11, 0, "-"        },
+    { 0x12, 0, "x"        },
+    { 0x13, 0, ":"        },
+    { 0x14, 0, "<->"      },
+    { 0x15, 0, "F 10^x"   },
+    { 0x16, 0, "F e^x"    },
+    { 0x17, 0, "F lg"     },
+    { 0x18, 0, "F ln"     },
+    { 0x19, 0, "F arcsin" },
+    { 0x1A, 0, "F arccos" },
+    { 0x1B, 0, "F arctg"  },
+    { 0x1C, 0, "F sin"    },
+    { 0x1D, 0, "F cos"    },
+    { 0x1E, 0, "F tg"     },
+    { 0x20, 0, "F пи"     },
+    { 0x21, 0, "F корень" },
+    { 0x22, 0, "F x^2"    },
+    { 0x23, 0, "F 1/x"    },
+    { 0x24, 0, "F x^y"    },
+    { 0x25, 0, "F ->"     },
+    { 0x26, 0, "K МГ"     },
+    { 0x27, 0, "K -"      },
+    { 0x28, 0, "K x"      },
+    { 0x29, 0, "K :"      },
+    { 0x2A, 0, "K МЧ"     },
+    { 0x30, 0, "K ЧМ"     },
+    { 0x31, 0, "K |x|"    },
+    { 0x32, 0, "K ЗН"     },
+    { 0x33, 0, "K ГМ"     },
+    { 0x34, 0, "K [x]"    },
+    { 0x35, 0, "K {x}"    },
+    { 0x36, 0, "K max"    },
+    { 0x37, 0, "K /\\"    },
+    { 0x38, 0, "K \\/"    },
+    { 0x39, 0, "K (+)"    },
+    { 0x3A, 0, "K ИНВ"    },
+    { 0x3B, 0, "K СЧ"     },
+    { 0x40, 1, "xП"       },
+    { 0x50, 0, "С/П"      },
+    { 0x51, 2, "БП"       },
+    { 0x52, 0, "В/О"      },
+    { 0x53, 2, "ПП"       },
+    { 0x54, 0, "K НОП"    },
+    { 0x55, 0, "K 1"      },
+    { 0x56, 0, "K 2"      },
+    { 0x57, 2, "F x#0"    },
+    { 0x58, 2, "F L2"     },
+    { 0x59, 2, "F x>=0"   },
+    { 0x5A, 2, "F L3"     },
+    { 0x5B, 2, "F L1"     },
+    { 0x5C, 2, "F x<0"    },
+    { 0x5D, 2, "F L0"     },
+    { 0x5E, 2, "F x=0"    },
+    { 0x60, 1, "Пx"       },
+    { 0x70, 1, "K x#0"    },
+    { 0x80, 1, "K БП"     },
+    { 0x90, 1, "K x>=0"   },
+    { 0xA0, 1, "K ПП"     },
+    { 0xB0, 1, "K xП"     },
+    { 0xC0, 1, "K x<0"    },
+    { 0xD0, 1, "К Пx"     },
+    { 0xE0, 1, "K x=0"    },
+    { 0 },
 };
 
 static const char hex_char[16] = "0123456789ABCDEF";
@@ -120,7 +126,7 @@ char *decompile (unsigned opcode, int *address_flag)
     } else {
         for (p=opcodes; p->mnemonics; p++) {
             if (p->type == 1) {
-                if (opcode/16 == p->opcode/16 && opcode%16 < 15) {
+                if ((opcode >> 4) == (p->opcode >> 4) && (opcode & 15) < 15) {
                     strcpy (cmd, p->mnemonics);
                     strcat (cmd, " ");
                     strcat (cmd, hex_string[opcode & 15]);
@@ -145,3 +151,43 @@ char *decompile (unsigned opcode, int *address_flag)
     }
     return cmd;
 }
+
+#ifdef TEST
+#include <stdio.h>
+
+int main()
+{
+    int opcode, i;
+    opcode_t *p;
+
+    printf ("^ Opcode ");
+    for (i=0; i<=0xf; i++)
+        printf ("^ %X ", i);
+    printf ("^\n");
+    for (opcode=0; opcode<=0xff; opcode++) {
+        if ((opcode & 0xf) == 0)
+            printf ("^ %X ", opcode >> 4);
+        for (p=opcodes; p->mnemonics; p++) {
+            if (p->type == 1) {
+                if ((opcode >> 4) == (p->opcode >> 4) && (opcode & 15) < 15) {
+                    printf ("| <html>%s</html> %X ", p->mnemonics, opcode & 15);
+                    break;
+                }
+            } else if (opcode == p->opcode) {
+                printf ((p->type == 2) ? "^ " : "| ");
+                printf ("<html>%s</html> ", p->mnemonics);
+                break;
+            }
+        }
+        if (! p->mnemonics) {
+            if (opcode <= 9)
+                printf ("| %X ", opcode);
+            else
+                printf ("|  ");
+        }
+        if ((opcode & 0xf) == 0xf)
+            printf ("|\n");
+    }
+    return 0;
+}
+#endif
