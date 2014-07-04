2795b402cf92c4c60128e53799c49b18890fa8ef
serge.vakulenko <serge.vakulenko@f4aa9954-0cdb-11de-b8f3-b9fb9eb71b04>
2014-07-04 04:36:00 +0000

    Wizard example: set Mac style.

--- trunk/utilities/qtwizard2/trivialwizard.cpp
+++ trunk/utilities/qtwizard2/trivialwizard.cpp
@@ -113,6 +113,7 @@ int main(int argc, char *argv[])
     wizard.addPage(createConclusionPage());
 
     wizard.setWindowTitle("Trivial Wizard");
+    wizard.setWizardStyle(QWizard::MacStyle);
 #ifdef Q_OS_SYMBIAN
     wizard.showMaximized();
 #else
