3d173b799319df28fc9973fa8b996067a5f29525
serge.vakulenko <serge.vakulenko@f4aa9954-0cdb-11de-b8f3-b9fb9eb71b04>
2014-08-12 03:02:46 +0000

    Ufstool: fsdb compiled.

--- utilities/ufstool/fsdb_ffs/fsdb.c
+++ utilities/ufstool/fsdb_ffs/fsdb.c
@@ -28,11 +28,6 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef lint
-static const char rcsid[] =
-  "$FreeBSD$";
-#endif /* not lint */
-
 #include <sys/param.h>
 #include <ctype.h>
 #include <err.h>
@@ -42,7 +37,6 @@ static const char rcsid[] =
 #include <stdint.h>
 #include <string.h>
 #include <time.h>
-#include <timeconv.h>
 
 #include <ufs/ufs/dinode.h>
 #include <ufs/ufs/dir.h>
@@ -1140,6 +1134,48 @@ badformat:
 	return 1;
     }
     return 0;
+}
+
+/*
+ * Convert a 32 bit representation of time_t into time_t.  XXX needs to
+ * implement the 50-year rule to handle post-2038 conversions.
+ */
+time_t
+_time32_to_time(__int32_t t32)
+{
+    return((time_t)t32);
+}
+
+/*
+ * Convert time_t to a 32 bit representation.  If time_t is 64 bits we can
+ * simply chop it down.   The resulting 32 bit representation can be
+ * converted back to a temporally local 64 bit time_t using time32_to_time.
+ */
+__int32_t
+_time_to_time32(time_t t)
+{
+    return((__int32_t)t);
+}
+
+/*
+ * Convert a 64 bit representation of time_t into time_t.  If time_t is
+ * represented as 32 bits we can simply chop it and not support times
+ * past 2038.
+ */
+time_t
+_time64_to_time(__int64_t t64)
+{
+    return((time_t)t64);
+}
+
+/*
+ * Convert time_t to a 64 bit representation.  If time_t is represented
+ * as 32 bits we simply sign-extend and do not support times past 2038.
+ */
+__int64_t
+_time_to_time64(time_t t)
+{
+    return((__int64_t)t);
 }
 
 CMDFUNCSTART(chbtime)
