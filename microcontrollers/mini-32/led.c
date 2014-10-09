4ea9a28c5de56b79372e158e560d4893e84a90cd
serge.vakulenko <serge.vakulenko@f4aa9954-0cdb-11de-b8f3-b9fb9eb71b04>
2014-10-09 06:02:06 +0000

    Added copyright for mini32 example.

--- microcontrollers/mini-32/led.c
+++ microcontrollers/mini-32/led.c
@@ -1,5 +1,20 @@
 /*
- * Include processor definitions.
+ * LED blinking example for mikroE MINI-32 board.
+ *
+ * Copyright (C) 2012 Serge Vakulenko, <serge@vak.ru>
+ *
+ * This file is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * You can redistribute this file and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software Foundation;
+ * either version 2 of the License, or (at your discretion) any later version.
+ * See the accompanying file "COPYING.txt" for more details.
+ *
+ * As a special exception to the GPL, permission is granted for additional
+ * uses of the text contained in this file.  See the accompanying file
+ * "COPY-UOS.txt" for details.
  */
 #include "pic32mx.h"
 
